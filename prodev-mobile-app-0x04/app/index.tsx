0511cec415aeab4019049ff4dbbd05a8cb4145cb
LeonNjoroge <leonmuraguri@gmail.com>
2025-07-27 18:35:09 +0300

    Feat: Complete task 0

--- prodev-mobile-app-0x04/app/index.tsx
+++ prodev-mobile-app-0x04/app/index.tsx
@@ -1,67 +1,10 @@
-import { Text, View, ImageBackground, Dimensions, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { Text, View, ImageBackground, TouchableOpacity, Image } from "react-native";
 import { SafeAreaView, SafeAreaProvider} from "react-native-safe-area-context";
-
+import {styles} from "@/app/styles/_mainstyle";
+import {BACKGROUNDIMAGE, HEROLOGO} from "@/app/constants";
 
 export default function Index() {
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-        },
-        background: {
-            flex: 1,
-            justifyContent: "center",
-            width: "100%",
-            height: Dimensions.get("window").height,
-        },
-        companyLogo: {
-            width: "100%",
-            alignItems: "center",
-            padding: 20,
-            marginBottom: 50,
-        },
-        textGroup: {
-            alignItems: "center",
-        },
-        textLarge: {
-            color: "white",
-            fontWeight: "800",
-            fontSize: 40,
-            textAlign: "center",
-            marginBottom: 12,
-        },
-        textSmall: {
-            color: "white",
-            fontSize: 18,
-            fontWeight: "200",
-            textAlign: "center",
-        },
-        transparentButton: {
-            borderColor: "white",
-            borderWidth: 2,
-            borderRadius: 40,
-            paddingVertical: 15,
-            paddingHorizontal: 5,
-            alignItems: "center",
-            fontSize: 20,
-            flex: 1,
-        },
-        button: {
-            borderColor: "white",
-            borderWidth: 2,
-            borderRadius: 40,
-            paddingVertical: 15,
-            paddingHorizontal: 5,
-            alignItems: "center",
-            fontSize: 20,
-            backgroundColor: "white",
-            flex: 1,
-        },
-        buttonGroup: {
-            flexDirection: "row",
-            gap: 20,
-            paddingHorizontal: 20,
-        },
-    });
+
 
     return (
         <SafeAreaProvider>
@@ -69,25 +12,27 @@ export default function Index() {
 
                 <ImageBackground
                     // source={require("@/assets/images/background-image.png")}
-                    style={styles.background}
+                    source={BACKGROUNDIMAGE}
+                    style={styles.backgroundImageContainer}
                     resizeMode="cover"
                 >
-                    <View style={styles.companyLogo}>
+                    <View style={styles.logoContainer}>
                         {/*<Image source={require("@/assets/images/Logo.png")} />*/}
+                        <Image source={HEROLOGO} />
                     </View>
-                    <View style={styles.textGroup}>
-                        <Text style={styles.textLarge}>Find your favorite place here</Text>
-                        <Text style={styles.textSmall}>The best prices for over 2 </Text>
-                        <Text style={styles.textSmall}>million properties worldwide</Text>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.titleSubText}>Find your favorite place here</Text>
+                        <Text style={styles.titleSubText}>The best prices for over 2 </Text>
+                        <Text style={styles.titleSubText}>million properties worldwide</Text>
                     </View>
 
                     <View style={styles.buttonGroup}>
-                        <TouchableOpacity style={styles.button}>
-                            <Text style={{ ...styles.textSmall, color: "black" }}>Join here</Text>
+                        <TouchableOpacity style={styles.buttonPrimary}>
+                            <Text style={{ ...styles.buttonPrimaryText, color: "black" }}>Join here</Text>
                         </TouchableOpacity>
 
-                        <TouchableOpacity style={styles.transparentButton}>
-                            <Text style={styles.textSmall}>Sign In</Text>
+                        <TouchableOpacity style={styles.buttonPrimary}>
+                            <Text style={styles.buttonPrimaryText}>Sign In</Text>
                         </TouchableOpacity>
                     </View>
 
